4a7774730e473758cda306a4205df09e176db3e3
Evan Kaplan <ekaplan6@gatech.edu>
2016-09-25 01:05:31 -0400

    working on service2

--- Python/service2.py
+++ Python/service2.py
@@ -5,3 +5,20 @@ Created on Sun Sep 25 00:12:43 2016
 @author: evankaplan
 """
 
+import pandas as pd
+import quandl as quandl
+import matplotlib.pyplot as plt
+
+def service2(stockName):
+    df = quandl.get("YAHOO/{}".format(stockName), authtoken="p69UHReX9R_5Gr1hKavd")
+    df.drop(df.columns[[0,1,2,3,4]], axis=1, inplace=True)
+    
+    df['Daily Return'] = (df / df.shift(1)) - 1
+    
+    drvalues = df['Daily Return'].values
+    drvalues = drvalues[1:]
+    
+    adjCloseValues = df['Adjusted Close'].values
+    adjCloseValues = adjCloseValues[1:]
+    
+    mx = ma.masked_array(x, mask=[0, 0, 0, 1, 0])
