dafa1c0d3fde21b89d0258c6da40b2591adc43f1
Evan Kaplan <ekaplan6@gatech.edu>
2016-09-25 04:00:18 -0400

    Got sevice2 running well

--- Python/service2.py
+++ Python/service2.py
@@ -9,7 +9,7 @@ import pandas as pd
 import quandl as quandl
 import matplotlib.pyplot as plt
 
-def service2(stockName):
+def service2(stockName, percentchange):
     df = quandl.get("YAHOO/{}".format(stockName), authtoken="p69UHReX9R_5Gr1hKavd")
     df.drop(df.columns[[0,1,2,3,4]], axis=1, inplace=True)
     
@@ -21,4 +21,34 @@ def service2(stockName):
     adjCloseValues = df['Adjusted Close'].values
     adjCloseValues = adjCloseValues[1:]
     
-    mx = ma.masked_array(x, mask=[0, 0, 0, 1, 0])
+    neverEnd = 0
+    instanceOfEqualPerc = 0
+    numberOfDays = 0
+    array = []
+    for x in range(len(drvalues)):
+        if percentchange <= drvalues[x] + .0025 and percentchange >= drvalues[x] - .0025:
+            instanceOfEqualPerc = instanceOfEqualPerc + 1
+            currentCloseValue = adjCloseValues[x - 1]
+            dayNumber = x
+            runner = True
+            while(dayNumber < len(adjCloseValues) and adjCloseValues[dayNumber] < currentCloseValue and runner):
+                dayNumber = dayNumber + 1
+                if dayNumber == len(adjCloseValues) - 1:
+                    neverEnd = neverEnd + 1
+                    runner = False
+                    dayNumber = x
+            array.append(dayNumber - x)
+            print "number of days:  " + str(dayNumber - x)
+            numberOfDays = dayNumber - x + numberOfDays
+            
+    avgDays = numberOfDays/(instanceOfEqualPerc - neverEnd)
+    
+    plt.hist(array, 500)
+    
+    print avgDays    
+    print numberOfDays
+    print instanceOfEqualPerc
+    print neverEnd
+    
+service2("NVAX", -.01)
+            
